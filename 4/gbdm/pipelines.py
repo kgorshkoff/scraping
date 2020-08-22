0663ffb146f70b2309db9c872cb054af52436ca4
Kirill Gorshkoff <gobzter@gmail.com>
2020-08-22 23:03:18 +0300

    done homework with itempipelines to save data and pillow/pytesseract stack to process phones

---
+++ 4/gbdm/pipelines.py
@@ -0,0 +1,34 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import pymongo
+
+
+class GbdmPipeline:
+    def __init__(self, mongo_uri, mongo_db, mongo_collection):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+        self.mongo_collection = mongo_collection
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=f'mongodb://{crawler.settings.get("MONGODB_SERVER")}:{crawler.settings.get("MONGODB_PORT")}',
+            mongo_db=crawler.settings.get('MONGODB_DB'),
+            mongo_collection=crawler.settings.get('MONGODB_COLLECTION')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.mongo_collection].update({'url': item['url']}, item, upsert=True)
+        return item
